a9555f5491780e019e82dfbc3b70c4484a3f1702
raie03 <raie.iwatsuki@gmail.com>
2024-12-23 19:45:05 +0900

    カードリストの作成

---
+++ app/components/BBSCard.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+
+const BBSCard = () => {
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Create project</CardTitle>
+          <CardDescription>
+            Deploy your new project in one-click.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente nemo
+          vel animi deleniti, assumenda ipsa fuga voluptas cumque dolorum error,
+          recusandae, nobis qui sequi eos iure eaque dignissimos dolorem ullam?
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Link href={"/bbs-posts/1"} className="text-blue-500">
+            Read More
+          </Link>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default BBSCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,9 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import Image from "next/image";
-import Link from "next/link";
+import BBSCardList from "./components/BBSCardList";
 
 export default function Home() {
   return (
     <main>
-      <Card>
-        <CardHeader>
-          <CardTitle>Create project</CardTitle>
-          <CardDescription>
-            Deploy your new project in one-click.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente nemo
-          vel animi deleniti, assumenda ipsa fuga voluptas cumque dolorum error,
-          recusandae, nobis qui sequi eos iure eaque dignissimos dolorem ullam?
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          <Link href={"/bbs-posts/1"}>Read More</Link>
-        </CardFooter>
-      </Card>
+      <BBSCardList />
     </main>
   );
 }
