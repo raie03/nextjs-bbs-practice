0bef892ea525e569b459456d56d33d58eaf46861
raie03 <raie.iwatsuki@gmail.com>
2024-12-23 22:47:25 +0900

    投稿機能の作成

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -5,3 +5,15 @@ export async function GET(req: Request) {
     const allBBSPosts = await prisma.post.findMany();
     return NextResponse.json(allBBSPosts);
 }
+export async function POST(req: Request) {
+    const {username, title, content} = await req.json();
+
+    const post = await prisma.post.create({
+        data:{
+            username,
+            title,
+            content,
+        },
+    });
+    return NextResponse.json(post);
+}
